6edad5f956153611f8deacebe8ca54aa79460560
Dan Chavez <121655211+dnachavez@users.noreply.github.com>
2023-08-29 00:17:43 +0800

    Revert Model

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def get_reply(input_string):
     """
     try:
         response = openai.ChatCompletion.create(
-            model = "pai-001-light-beta",
+            model = "gpt-3.5-turbo",
             max_tokens = 1000,
             messages = [
                 {"role": "system", "content": "You are a helpful assistant."},
